9c3bcb97443d88996f25bbf29dfa115c8c0c015a
platogm <platogebremedhingizaw@gmail.com>
2022-04-07 00:42:13 -0700

    Fixed bugs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints a string using printf
+ * main - Prints quote
  *
- * Return - zero on success
+ * Return: zero on success
  *
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		return (0);
 }
