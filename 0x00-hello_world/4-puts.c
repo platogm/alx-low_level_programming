9f8285abdfd7a12f2e50cb32fdf3bab4348deb7c
platogm <platogebremedhingizaw@gmail.com>
2022-04-07 00:40:14 -0700

    Fixed bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints a string on STDOUT and return zero 
+ * main - Prints quote
  *
  * Return: zero on success
+ *
  */
-
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
