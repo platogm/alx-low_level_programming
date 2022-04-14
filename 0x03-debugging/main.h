391b36b46377ef04e9f3e79aab9c22a43f189aea
platogm <platogebremedhingizaw@gmail.com>
2022-04-13 21:44:32 -0700

    Added documentaion on main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - checks if the number is pos or not
+ * @i: the number to be checked
+ * Return: nothing
+ */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
